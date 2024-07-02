1eb8d78b95450cfd2636a735f81312d597949b87
huy-xu <doraemonrx@gmail.com>
2024-07-02 23:14:55 +0700

    Q3

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -6,4 +6,23 @@ class BoardsController < ApplicationController
   def show
     @board = Board.find(params[:id])
   end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.new(board_params)
+    if @board.save
+      redirect_to @board, notice: '掲示板が作成されました。'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:title, :description, :author)
+  end
 end
--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,6 @@
 class Board < ApplicationRecord
   validates :title, presence: true, uniqueness: true, length: { maximum: 30 }
   validates :description, presence: true, length: { maximum: 300 }
+  validates :author, presence: true, length: { maximum: 10 }
   has_many :comments, dependent: :destroy
 end
