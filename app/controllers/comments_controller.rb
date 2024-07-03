c9f7ba46a10e7446a98f1b6f1d03e4f8b35a1acb
huy-xu <doraemonrx@gmail.com>
2024-07-03 22:54:35 +0700

    Q7

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,24 @@
 class CommentsController < ApplicationController
+  def create
+    @board = Board.find(params[:board_id])
+    @comment = @board.comments.build(comment_params)
+    if @comment.save
+      redirect_to @board, notice: 'コメントが追加されました。'
+    else
+      redirect_to @board, alert: 'コメントの追加に失敗しました。'
+    end
+  end
+
+  def destroy
+    @board = Board.find(params[:board_id])
+    @comment = @board.comments.find(params[:id])
+    @comment.destroy
+    redirect_to @board, notice: 'コメントが削除されました。'
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
